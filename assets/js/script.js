079699ba0762359c85fbf69395244190486f2889
sabbirevan31 <evan.cnits@gmail.com>
2023-12-25 15:41:33 +0600

    about section

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,21 @@ $("#slider").owlCarousel({
       },
     },
   });
+
+$("#slider1").owlCarousel({
+    loop: true,
+    margin: 15,
+    nav: false,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  });
